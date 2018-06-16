0a32a80cbb763dfc67b6749a7674b8a44c4bd1fb
Abhishek <abhishekcs139@gmail.com>
2018-06-16 16:01:43 +0530

    added feautre2

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $qc = new QRCODE();
 // create text QR code 
 $qc->URL("https://github.com/payward/kraken-api-client/blob/master/php/KrakenAPIClient.php"); 
 
+echo "this is feature2";
 // render QR code
 echo "demo";
 $qc->QRCODE(400,"sample.png");
